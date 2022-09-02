f5c771618ed7d41277636f5ee25ea46728db41ab
Nerakma84 <fnk8476@gmail.com>
2022-09-01 22:29:24 -0500

    working on local storage

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var currentHour = moment().hour();
  console.log(currentHour) //sets current hour//
 //jquery loop thru all textareas//
 $('.description').each(function(){
-    var timeBlock = parseInt($(this).attr('id'))
+    var timeBlock = parseInt($(this).attr('id')) //parses a string argument and returns an integer
     console.log(timeBlock)
     if (timeBlock < currentHour){
         $(this).addClass('past')
@@ -15,3 +15,26 @@ $('.description').each(function(){
         $(this).addClass('future')
     }
 })
+
+$(".saveBtn").on("click", function() {
+    let value = $(this).siblings(".description").val();
+    let timeEl = $(this).parent().attr("id");
+    localStorage.setItem(timeEl, value);
+    })
+
+
+$('#hour8 .description').val(localStorage.getItem("hour8"));
+$('#hour9 .description').val(localStorage.getItem("hour9"));
+$('#hour10 .description').val(localStorage.getItem("hour10"));
+$('#hour11 .description').val(localStorage.getItem("hour11"));
+$('#hour12 .description').val(localStorage.getItem("hour12"));
+$('#hour13 .description').val(localStorage.getItem("hour13"));
+$('#hour14 .description').val(localStorage.getItem("hour14"));
+$('#hour15 .description').val(localStorage.getItem("hour15"));
+$('#hour16 .description').val(localStorage.getItem("hour16"));
+$('#hour17 .description').val(localStorage.getItem("hour17"));
+
+
+
+
+
